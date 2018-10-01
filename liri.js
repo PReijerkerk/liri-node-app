0d2672820737fe427739f904f7124780a4d72916
P Reijerkerk <preijerkerk@gmail.com>
2018-10-01 18:38:53 -0400

    added in a basic call to the Spotify API and returns the response

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+//requires the NPM modules for accessing the spotify API
+var Spotify = require('node-spotify-api');
+//requires the information contained in the keys.js file to access spotify API
+var SpotifyKeys = require('./keys.js');
+
+//sets spotify equal to the key info to call the spotify API
+var spotify = new Spotify(SpotifyKeys.spotify);
+
+//searches the spotify API by track name 
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    //console.logs the response from the Spotify API
+    console.log(response);
+  })
+  .catch(function(err) {
+    //console.logs any caught errors
+    console.log(err);
+  });
